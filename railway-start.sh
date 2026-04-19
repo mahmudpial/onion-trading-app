7a1dac5ea7147b04deed1413b4720cc27dd34937
pial <pialmahmud80@gmail.com>
2026-04-19 07:50:22 +0600

    delete all deploy file

--- railway-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan migrate --force
-php artisan storage:link
-php artisan serve --host=0.0.0.0 --port=$PORT
